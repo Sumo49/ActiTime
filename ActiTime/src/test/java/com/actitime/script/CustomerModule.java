5e1c2ddf2776b86e82c36e6ed8ca248637f80032
dell <dell@sumo>
2023-03-04 11:13:25 +0530

    Commit

---
+++ ActiTime/src/test/java/com/actitime/script/CustomerModule.java
@@ -0,0 +1,13 @@
+package com.actitime.script;
+
+import org.testng.Reporter;
+import org.testng.annotations.Test;
+
+public class CustomerModule {
+	
+	@Test
+	public static void testCreateCustomer() {
+		Reporter.log("CreateCustomer",true);
+	}
+
+}
